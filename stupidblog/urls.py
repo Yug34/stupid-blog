c256965448029cf385423c0d6c224a23bb7920e7
Yug34 <vertilax1337@gmail.com>
2020-05-29 23:57:18 +0530

    trying to fix the bug2

--- stupidblog/urls.py
+++ stupidblog/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('accounts/profile/', user_views.profile, name='profile'),
     path('password-reset/',
          auth_views.PasswordResetView.as_view(
              template_name='users/password_reset.html'
