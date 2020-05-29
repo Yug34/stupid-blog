b0c1358024f7fb3a9897721a2b0bb835c3b6ac9b
Yug34 <vertilax1337@gmail.com>
2020-05-29 15:28:35 +0530

    Initial Commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\yug\desktop\python\python projects\stupidblog\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
